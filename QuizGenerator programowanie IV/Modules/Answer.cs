c0e035e36cda8d7f1e9532db13a67bdb4f4504be
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2022-04-20 14:49:34 +0200

    feat/add-quiz-structure-and-file-handling

---
+++ QuizGenerator programowanie IV/Modules/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class Answer
+    {
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+
+        public Answer(string answerText, bool isCorrect)
+        {
+            this.AnswerText = answerText;
+            this.IsCorrect = isCorrect;
+        }
+    }
+}
---
+++ QuizGenerator programowanie IV/Modules/Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class Quiz
+    {
+        public string QuizName { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Quiz(string quizName, List<Question> questions)
+        {
+            this.QuizName = quizName;
+            this.Questions = questions;
+        }
+    }
+}
