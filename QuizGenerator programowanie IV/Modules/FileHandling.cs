307ff7ed34e90b39f1ee426f89f2b2ee38be73f6
jaworskig00 <2000grzegorz@gmail.com>
2022-04-20 14:45:40 +0200

    Added quiz structure and file handling class

---
+++ QuizGenerator programowanie IV/Modules/FileHandling.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class FileHandling
+    {
+        public void SaveToFile(Quiz quiz)
+        {
+            string json = JsonSerializer.Serialize(quiz);
+
+            File.WriteAllText($"C:\\Users\\2000g\\OneDrive\\Pulpit\\{quiz.QuizName}.txt", json);
+        }
+    }
+}
---
+++ QuizGenerator programowanie IV/Modules/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class Question
+    {
+        public int QuistionNumber { get; set; }
+        public string QuestionText { get; set; }
+        public int Time { get; set; }
+        public List<Answer> Answers { get; set; }
+
+        public Question(int questionNumber, string questionText, int time, List<Answer> answers)
+        {
+            this.QuistionNumber = questionNumber;
+            this.QuestionText = questionText;
+            this.Time = time;
+            this.Answers = answers;
+        }
+    }
+}
