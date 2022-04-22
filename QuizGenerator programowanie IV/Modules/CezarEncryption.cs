622245ec7a491ec3483acd39c01e6f93addf24a8
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2022-04-22 18:27:09 +0200

    feat/cezar-encryption
    - Added cezar encryption class with 2 methods:
    - - EncryptFile for encryption before saving to file
    - - DecryptFile for decrypting file before reading data from it

---
+++ QuizGenerator programowanie IV/Modules/CezarEncryption.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class CezarEncryption : IEncryption
+    {
+        int key = 2; // można dodać żeby było to dodawane jako argument do klasy lub metod
+        public string EncryptText(string text)
+        {
+            char[] textToArray = text.ToCharArray();
+            string outputText = string.Empty;
+
+            for (int i = 0; i < textToArray.Length; i++)
+            {
+                // jeśli chcemy żeby szyfrowane byly tylko litery
+                //if (!char.IsLetter(textToArray[i]))
+                //{
+                //    continue;
+                //}
+                outputText += (char)(textToArray[i] + key);
+            }
+
+            return outputText;
+
+        }
+
+        public string DecryptText(string text)
+        {
+            char[] textToArray = text.ToCharArray();
+            string outputText = string.Empty;
+
+            for (int i = 0; i < textToArray.Length; i++)
+            {
+                // jeśli chcemy żeby szyfrowane byly tylko litery
+                //if (!char.IsLetter(textToArray[i]))
+                //{
+                //    continue;
+                //}
+                outputText += (char)(textToArray[i] - key);
+            }
+            return outputText;
+        }
+    }
+}
---
+++ QuizGenerator programowanie IV/Modules/IEncryption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    public interface IEncryption
+    {
+        string EncryptText(string text);
+        string DecryptText(string text);
+    }
+}
